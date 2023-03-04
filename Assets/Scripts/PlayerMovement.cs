b678f7e9b3eecb42acba8247b017d57675265515
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-03-04 18:09:42 +0500

    Добавил input system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(GroundCheck))]
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
 {
 
@@ -13,8 +12,8 @@ public class PlayerMovement : MonoBehaviour
     public Transform cam;
     public float speed = 7f;
     public float turnTime = 0.1f;
-    public bool useRotation = true;
-    public bool rotateToCamera = false;
+    public bool rotateWhileMoving = true;
+    public bool rotateWithCamera = false;
 
     private float turnSmoothVelosity;
 
@@ -26,8 +25,10 @@ public class PlayerMovement : MonoBehaviour
     private bool readyToJump = true;
     private Rigidbody body;
     private GroundCheck gc;
+    private PlayerInput playerInput;
 
-
+    private Vector2 directionFlat;
+    private Vector3 direction;
     private float horizontal;
     private float vertial;
     private bool jumpPressed;
@@ -36,8 +37,8 @@ public class PlayerMovement : MonoBehaviour
     {
         body = GetComponent<Rigidbody>();
         gc = GetComponent<GroundCheck>();
+        playerInput = GetComponent<PlayerInput>();
     }
-
 
     private void Update()
     {
@@ -47,17 +48,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void ReadInputs()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertial = Input.GetAxisRaw("Vertical");
-        jumpPressed = Input.GetKey(jumpKey);
+        directionFlat = playerInput.actions["Move"].ReadValue<Vector2>();
+        direction = new Vector3(directionFlat.x, 0f, directionFlat.y).normalized;
+        jumpPressed = playerInput.actions["Jump"].triggered;
     }
-
 
     void FixedUpdate()
     {
-        Vector3 direction = new Vector3(horizontal, 0f, vertial).normalized;
-
-        if (rotateToCamera)
+        if (rotateWithCamera)
         {
             transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, cam.transform.localEulerAngles.y, transform.localEulerAngles.z);
         }
@@ -67,7 +65,7 @@ public class PlayerMovement : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             direction = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             body.AddForce(speed * 100f * Time.deltaTime * direction, ForceMode.Acceleration);
-            if (useRotation)
+            if (rotateWhileMoving)
             {
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,
                                                     targetAngle,
