42d10e85bf91c7e64eaf30b14f9909a429d9ace6
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-03-06 21:21:55 +0500

    упростил движение

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -8,19 +8,15 @@ public class GroundCheck : MonoBehaviour
     public float maxDistance;
 
     public bool OnGround { get; private set; }
-    public RaycastHit Hit { get; private set; }
-    public Vector3 RayDirection { get; private set; }
 
     private Ray downRay;
     private RaycastHit hit;
 
-    void Update()
+    private void Update()
     {
         downRay = new Ray(transform.position, Vector3.down);
-        RayDirection = transform.TransformDirection(Vector3.down);
         if (Physics.Raycast(downRay, out hit, maxDistance, considerGroundAs))
         {
-            Hit = hit;
             OnGround = true;
         }
         else
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,25 +11,20 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     public Transform cam;
     public float speed = 7f;
-    public float turnTime = 0.1f;
-
-    private float turnSmoothVelosity;
 
     [Header("Jumping")]
-    public KeyCode jumpKey = KeyCode.Space;
     public float jumpCooldown;
     public float jumpForce;
 
     [Header("Gravity")]
     public float gravityForce = -9.81f;
 
-    private bool readyToJump = true;
     private CharacterController controller;
     private GroundCheck gc;
     private PlayerReader playerReader;
 
-
     private Vector3 velocity;
+    private Vector3 moveDirection;
 
     void Awake()
     {
@@ -41,60 +36,63 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         playerReader.ReadInputs();
+        ApplyCameraAngles();
+        ApplyGravity();
+        ApplyJump();
+        ApplyMove();
 
-        // обновление направления движения в зависимости от направления камеры
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, cam.eulerAngles.y, transform.eulerAngles.z);
+        controller.Move(speed * Time.deltaTime * moveDirection +
+                        velocity * Time.deltaTime);
 
+    }
+
+    /// <summary>
+    /// Изменяет moveDirection в соответсвии с желаемым направлением движения
+    /// </summary>
+    private void ApplyMove()
+    {
+        if (playerReader.Direction.magnitude > 0.1f)
+        {
+            moveDirection = transform.right * playerReader.Direction.x +
+                            transform.forward * playerReader.Direction.z;
+        }
+        else
+        {
+            moveDirection = Vector3.zero;
+        }
+    }
+
+    /// <summary>
+    /// Изменяет velocity.y для осуществления прыжка
+    /// </summary>
+    private void ApplyJump()
+    {
+        if (controller.isGrounded && playerReader.JumpPressed)
+        {
+            velocity.y = jumpForce;
+        }
+    }
+
+    /// <summary>
+    /// Изменяет velocity.y, применяя к ней гравитационные силы
+    /// </summary>
+    private void ApplyGravity()
+    {
         if (controller.isGrounded)
         {
             velocity.y = -1f;
-
-            if (playerReader.JumpPressed)
-            {
-                velocity.y = jumpForce;
-            }
         }
         else
         {
             velocity.y += gravityForce * Time.deltaTime;
         }
-
-        if (playerReader.Direction.magnitude > 0.1f)
-        {
-            Vector3 moveDirection = transform.right * playerReader.Direction.x +
-                                    transform.forward * playerReader.Direction.z;
-            controller.Move(speed * Time.deltaTime * moveDirection);
-        }
-
-        controller.Move(velocity * Time.deltaTime);
-
     }
 
-    //private void HandleJump()
-    //{
-    //    if (playerReader.JumpPressed && readyToJump && gc.OnGround)
-    //    {
-    //        readyToJump = false;
-
-    //        Jump();
-
-    //        Invoke(nameof(ResetJump), jumpCooldown);
-    //    }
-    //}
-
-    //private void Jump()
-    //{
-    //    //body.velocity = new Vector3(body.velocity.x, 0f, body.velocity.z);
-    //    //body.AddForce(body.transform.up * jumpForce, ForceMode.Impulse);
-    //    if (gc.OnGround)
-    //    {
-    //        controller.Move(new Vector3(0f, jumpForce, 0f) * Time.deltaTime);
-    //    }
-        
-    //}
-
-    //private void ResetJump()
-    //{
-    //    readyToJump = true;
-    //}
+    /// <summary>
+    /// Обновляет направление движения в зависимости от направления камеры
+    /// </summary>
+    private void ApplyCameraAngles()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, cam.eulerAngles.y, transform.eulerAngles.z);
+    }
 }
