58e1b82bb1ebbd9feb8714774e11ff601f1f364a
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-03-16 20:50:13 +0500

    Сделал анимацию всплывающего окна и скрипт показа

---
+++ Assets/Scripts/Animations/PopUp.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Animations
+{
+    public class PopUp: MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private TextMeshProUGUI _mesh;
+        [SerializeField] private float _showTime = 1f;
+
+        private Queue<string> _queue = new();
+        private bool _animating;
+
+        public void Show(string popUpText)
+        {
+            _queue.Enqueue(popUpText);
+        }
+
+        public void FixedUpdate()
+        {
+            if (_animating) return;
+            
+            while (_queue.Count != 0)
+            {
+                _animating = true;
+                _mesh.text = _queue.Dequeue();
+                _animator.SetTrigger("Open");
+                Invoke(nameof(Hide), _showTime);
+            }
+        }
+
+        private void Hide()
+        {
+            _animator.SetTrigger("Close");
+            _animating = false;
+        }
+    }
+}
