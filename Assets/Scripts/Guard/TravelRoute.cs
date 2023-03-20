48d532660cc4fe59d42fa6b4dfed91d233457ef3
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-03-20 21:33:25 +0500

    Добавлена прорисовка маршрута

--- Assets/Scripts/Guard/TravelRoute.cs
+++ Assets/Scripts/Guard/TravelRoute.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace Guard
@@ -19,6 +21,19 @@ namespace Guard
             var point = points[_currentPoint];
             _currentPoint = (_currentPoint + 1) % points.Count;
             return point;
+        }
+
+        private void OnDrawGizmos()
+        {
+            for (var i = 0; i < points.Count; i++)
+            {
+                var point = points[i];
+                var next = points[(i + 1) % points.Count];
+                var pos = point.transform.position;
+                Gizmos.DrawLine(pos, next.transform.position);
+                pos.y += point.pointRadius*2;
+                Handles.Label(pos, i.ToString());
+            }
         }
     }
 }
