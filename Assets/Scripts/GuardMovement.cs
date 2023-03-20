5aff3d58bf4c55ec6589ef93ebf8e873de2056f2
Lavshyak <lavshyak@gmail.com>
2023-03-20 15:41:25 +0500

    Фиксы, код поправил

--- Assets/Scripts/GuardMovement.cs
+++ Assets/Scripts/GuardMovement.cs
@@ -1,53 +1,55 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
+// ReSharper disable StringLiteralTypo
 
+// по этому мануалу делал
 //https://www.red-gate.com/simple-talk/development/dotnet-development/pathfinding-unity-c/
 
-public class PathFinding : MonoBehaviour
+public class GuardMovement : MonoBehaviour
 {
+    //сюда просто обьекты добавить, и он будет патрулировать их
     public Transform pointsStorageToPatrol;
+    //кого ловить
     public Transform targetToCatch;
+    //пока что не ворк
     public float fieldOfView = 90;
+    //как близко игрок должен находиться, чтоб увидеть его
     public float maxRange = 35;
+    //навигация AI
+    public NavMeshAgent nav;
     
+    //вызывается когда игрок суперблизко к охраннику
     public UnityEvent onTargetCatched;
     
     [Header("Debug")]
-    public bool debug = false;
+    //выводить на экран если видит, ставить метку куда идет
+    public bool debug;
+    //текст на экран, мол найден охранником
     public TextMeshProUGUI text;
+    //телепортирует этот обьект туда, куда идет
     public Transform goesToThatPosition;
     
-    private NavMeshAgent nav;
-    private int destPoint;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        nav = GetComponent<NavMeshAgent>();
-    }
+    //номер обьекта, к которому идет
+    private int _destPoint;
 
-    //установить следующую точку как цель
+    /// <summary>
+    /// установить следующую точку как цель
+    /// </summary>
     void GoToNextPoint()
     {
         if (pointsStorageToPatrol.childCount == 0)
         {
             return;
         }
-        nav.destination = pointsStorageToPatrol.GetChild(destPoint).position;
+        nav.destination = pointsStorageToPatrol.GetChild(_destPoint).position;
         
         if (debug)
             goesToThatPosition.position = nav.destination;
         
-        destPoint = (destPoint + 1) % pointsStorageToPatrol.childCount;
+        _destPoint = (_destPoint + 1) % pointsStorageToPatrol.childCount;
     }
 
     public void RestartFirstLevel()
@@ -58,15 +60,17 @@ public class PathFinding : MonoBehaviour
     void FixedUpdate()
     {
         //расстояние до игрока
-        var heading = targetToCatch.position - this.transform.position;
-        if (heading.sqrMagnitude < maxRange * maxRange) { //если до игрока достаточно малое расстояние
-            if(debug)
-                text.text = "Вы замечены";
-            // Target is within range.
-            nav.destination = targetToCatch.position; //установка цели для преследования
-            if (debug)
-                goesToThatPosition.position = nav.destination;
+        var vectorToPlayer = targetToCatch.position - this.transform.position;
+        if (vectorToPlayer.sqrMagnitude < maxRange * maxRange) { //если до игрока достаточно малое расстояние
             
+            nav.destination = targetToCatch.position; //установка цели для преследования
+
+            if (debug)
+            {
+                text.text = "Вы замечены";
+                goesToThatPosition.position = nav.destination;
+            }
+
             if (!nav.pathPending && nav.remainingDistance < 1.5f) //если игрок суперблизко
             {
                 //игрок пойман
@@ -78,8 +82,9 @@ public class PathFinding : MonoBehaviour
             return;
         }
         
+        //до сюда доходит когда игрок не замечен
         text.text = "";
-        //до сюда доходит когда игрока рядом нет
+        
         if (!nav.pathPending && nav.remainingDistance < 0.5f) //если суперблизко к точке патрулирования
             GoToNextPoint();
     }
