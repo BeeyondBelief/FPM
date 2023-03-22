40be25760a4bc47ae510fde710fe4b7c6e3a0df8
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-03-22 21:55:29 +0500

    Добавлена система установки настроек из меню игры

---
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Menu
+{
+    public class OptionsMenu: MonoBehaviour
+    {
+        public static float VolumeLevel { get; private set; }
+        public static bool ToggleButtonsEnabled { get; private set; }
+        
+        public void SetVolume(float volume)
+        {
+            VolumeLevel = volume;
+        }
+
+        public void SetToggleButtonBehavior(bool useToggle)
+        {
+            ToggleButtonsEnabled = useToggle;
+        }
+    }
+}
