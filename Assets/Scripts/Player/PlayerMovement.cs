4d4ff7d5dfe5fd4124e6fd0719325bebbfbbfe03
Nikita Zorin <nikita00zorinnn@mail.ru>
2023-04-04 18:14:22 +0500

    Добавлено автоматическое создание и поиск главной камеры

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,7 @@ using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Game;
+using UnityEditor;
 
 
 namespace Player
@@ -34,6 +35,26 @@ namespace Player
         private float _characterNormalHeight;
         private Vector3 _characterCenter;
         private Vector3 _velocity;
+        
+        private void OnValidate()
+        {
+            var foundCamera = FindObjectOfType<Camera>();
+            if (foundCamera is null)
+            {
+                EditorApplication.delayCall += () =>
+                {
+                    var cameraObject = new GameObject("CameraRoot");
+                    cameraObject.AddComponent<Camera>();
+                    cameraObject.AddComponent<AudioListener>();
+                    cameraObject.AddComponent<CinemachineBrain>();
+                    cam = cameraObject.GetComponent<CinemachineBrain>();
+                };
+            }
+            else
+            {
+                cam = foundCamera.GetComponent<CinemachineBrain>();
+            }
+        }
 
         private void Awake()
         {
